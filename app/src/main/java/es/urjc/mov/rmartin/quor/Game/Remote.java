b9a50ee0d14f52bfbb39c92a22b5ac56e477dfe3
ralvarezmar <r.alvarezmar@gmail.com>
2018-05-23 10:39:52 +0200

    Fixed errors

--- app/src/main/java/es/urjc/mov/rmartin/quor/Game/Remote.java
+++ app/src/main/java/es/urjc/mov/rmartin/quor/Game/Remote.java
@@ -17,8 +17,8 @@ import es.urjc.mov.rmartin.quor.Graphic.Status;
 public class Remote extends Player{
     private String nick;
     private Socket s;
-    //private final String IP="10.3.49.35";
-    private final String IP="192.168.1.7";
+    private final String IP="10.1.128.237";
+    //private final String IP="192.168.1.7";
     //private final String IP="10.0.2.2";
     private final int PORT=2020;
     public DataInputStream in;
